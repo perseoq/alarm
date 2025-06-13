72c516d3c1f0564c7349b584b6292d02bcb09c87
Rodrigo <rodrigoqyt@gmail.com>
2025-06-13 14:04:39 -0700

    Update alarm-alarm-installer.sh

--- v1.0/alarm-alarm-installer.sh
+++ v1.0/alarm-alarm-installer.sh
@@ -5,5 +5,8 @@ sudo mkdir -p /opt/alarm-alarm
 tar -xvf alarm-alarm.tar.xz
 cd alarm-alarm
 sudo chmod +x alarm
+cd ..
 sudo mv alarm-alarm/ /opt/
 sudo cp /opt/alarm-alarm/alarm-alarm.desktop /usr/share/applications/
+
+
