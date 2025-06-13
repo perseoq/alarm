cb1e8ba01d225d99933f21dc1d216e13247ab5b2
Rodrigo <rodrigoqyt@gmail.com>
2025-06-13 13:21:39 -0700

    Create alarm-alarm-installer.sh

---
+++ v1.0/alarm-alarm-installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -L -o alarm-alarm-v1.2.tar.xz https://github.com/perseoq/alarm/releases/download/study.alarm/alarm-alarm-v1.2.tar.xz
+sudo mkdir -p /opt/alarm-alarm
+sudo tar -xJf alarm-alarm-v1.2.tar.xz -C /opt/alarm-alarm --strip-components=1
+sudo chmod +x /opt/alarm-alarm/alarm
+sudo cp /opt/alarm-alarm/alarm-alarm.desktop /usr/share/applications/
