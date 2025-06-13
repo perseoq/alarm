752899b74501a568016643cf06ff6824d41c965d
Rodrigo <rodrigoqyt@gmail.com>
2025-06-13 13:35:41 -0700

    Update alarm-alarm-installer.sh

--- v1.0/alarm-alarm-installer.sh
+++ v1.0/alarm-alarm-installer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-curl -L -o alarm-alarm-v1.2.tar.xz https://github.com/perseoq/alarm/releases/download/study.alarm/alarm-alarm.tar.xz
+curl -L -o alarm-alarm.tar.xz https://github.com/perseoq/alarm/releases/download/study.alarm/alarm-alarm.tar.xz
 sudo mkdir -p /opt/alarm-alarm
-sudo tar -xJf alarm-alarm-v1.2.tar.xz -C /opt/alarm-alarm --strip-components=1
-sudo chmod +x /opt/alarm-alarm/alarm
+tar -xvf alarm-alarm.tar.xz
+cd alarm-alarm
+sudo chmod +x alarm
+sudo mv alarm-alarm/ /opt/
 sudo cp /opt/alarm-alarm/alarm-alarm.desktop /usr/share/applications/
